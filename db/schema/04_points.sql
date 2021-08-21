47883d8ba7a037e4fb01324ce71facc415b59ef5
Jonathan Choi <jonchoi11@gmail.com>
2021-08-21 02:43:33 +0000

    started databases (users, faves, maps, points). deleted widget.sql since it is not needed.

---
+++ db/schema/04_points.sql
@@ -0,0 +1,12 @@
+DROP TABLE IF EXISTS points CASCADE;
+
+CREATE TABLE points (
+  id SERIAL PRIMARY KEY,
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  map_id INTEGER REFERENCES maps(id) ON DELETE CASCADE,
+  title VARCHAR(255) NOT NULL DEFAULT 'point',
+  description TEXT,
+  latitude DOUBLE PRECISION, -- double precison will round the value of lat and long since those values are longer
+  longitute DOUBLE PRECISION,
+  image VARCHAR(255)
+)
