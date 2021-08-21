1b7dba1525e0c57511f2420a2e62167397aeab33
Jonathan Choi <jonchoi11@gmail.com>
2021-08-21 03:30:09 +0000

    added fake data for points

---
+++ db/seeds/04_points.sql
@@ -0,0 +1,5 @@
+INSERT INTO points (user_id, map_id, title, description, latitude, longitude, image)
+VALUES
+(1, 1, 'Pizza', 'Best pizza in the city', 49.25737, -123.12107, 'https://tryhiddengems.com/wp-content/uploads/2017/11/cororoypiecompany3-1024x576.jpg'),
+(2, 2, 'The Chief', 'Best hike in squamish', 49.68328, -123.14423, 'https://www.vancouvertrails.com/images/hikes-small/stawamus-chief.jpg'),
+(3, 3, 'Drinks', 'Best pub downtown', 49.28400, -123.10620, 'https://www.vmcdn.ca/f/files/via/images/buildings/lamplighter-public-house-vancouver.jpg;w=960');
