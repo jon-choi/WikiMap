30194c15501a7b4d877a610b9dc685c8f577b5fb
Erminio <Erminio>
2021-08-21 21:51:02 +0000

    change files

--- routes/map-routes.js
+++ routes/map-routes.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const router = express.Router();
-
+const { getMaps } = require('../db/map-queries');
 
 //GET /maps
-router.get('', (req, res) => {
-
+router.get('/', (req, res) => {
+  getMaps()
+    .then((maps) => {
+      res.json(maps);
+    });
 });
+
+//GET /maps/:map_id
+// router.get(`/:map_id`, (req, res) => {
+//   getMapById()
+//     .then((map_id) => {
+//       res.json(map_id);
+//     })
+// })
 
 module.exports = router;
--- server.js
+++ server.js
@@ -33,13 +33,17 @@ app.use(express.static("public"));
 
 // Separated Routes for each Resource
 // Note: Feel free to replace the example routes below with your own
-const usersRoutes = require("./routes/users");
-const widgetsRoutes = require("./routes/widgets");
+
+
+const mapRouter = require("./routes/map-routes");
+//const usersRoutes = require("./routes/users");
+//const widgetsRoutes = require("./routes/widgets");
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
-app.use("/api/users", usersRoutes(db));
-app.use("/api/widgets", widgetsRoutes(db));
+app.use('/maps', mapRouter);
+// app.use("/api/users", usersRoutes(db));
+// app.use("/api/widgets", widgetsRoutes(db));
 // Note: mount other resources here, using the same pattern above
 
 
