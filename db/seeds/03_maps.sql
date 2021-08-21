7b5a9880679e13488036f2f7aa33fbc8cfe03e35
Jonathan Choi <jonchoi11@gmail.com>
2021-08-21 03:16:39 +0000

    added fake data for maps and deleted widgets.sql

---
+++ db/seeds/03_maps.sql
@@ -0,0 +1,5 @@
+INSERT INTO maps (user_id, title, desciption)
+VALUES
+(1, 'Best Restaurants', 'All the best places in Vancouver'),
+(2, 'Best Hikes', 'Every hike in greater Vancouver area'),
+(3, 'Best Pubs', 'All the craziest pubs downtown');
